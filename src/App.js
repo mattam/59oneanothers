b261edc3ba7f26ac026881dffffb3d22b31d09b1
Matthew Chan <mattam@gmail.com>
2022-09-11 21:56:51 -0700

    Adding ESV copyright and adjusting footer styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div className="App">
       <h3>Testing out app</h3>
+
+      <div>
+        Verses
+      </div>
+      <footer>
+        <p>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. The ESV text may not be quoted in any publication made available to the public by a Creative Commons license. The ESV may not be translated into any other language.</p>
+        <p>Users may not copy or download more than 500 verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
+      </footer>
     </div>
   );
 }
