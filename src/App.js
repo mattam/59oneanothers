2d9677d483ced9adc5e3e49b31c56eb83944a738
Matthew Chan <mattam@gmail.com>
2022-09-11 22:16:23 -0700

    Adjusting code to dynamically pull the verses from the verses.json file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import "./App.css";
 
+import verses from './data/verses.json';
+
+const renderVerses = () => {
+  return verses.map( 
+      (verseObj, index) => 
+        <div className="verseObj" key={index}>
+          <span className="verses">&ldquo;{verseObj.verses}&rdquo;</span>
+          <span className="reference">{verseObj.reference}</span>
+        </div>
+    );
+}
+
+
 function App() {
   return (
     <div className="App">
       <h2>59 One Anothers</h2>
       <h4>59 Bible Verses that refer to how we are to treat one another</h4>
 
-      <div>
-        <span className="verses">&ldquo;Salt is good, but if the salt has lost its saltiness, how will you make it salty again? Have salt in yourselves, and be at peace with one another.&rdquo;</span>
-        <span className="reference">Mark 9:50</span>
-      </div>
+      {renderVerses()}
 
       <footer>
         <p>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. The ESV text may not be quoted in any publication made available to the public by a Creative Commons license. The ESV may not be translated into any other language.</p>
