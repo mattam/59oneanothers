d334c3cedffcef06f83d98749447d63cf99d832c
Matthew Chan <mattam@gmail.com>
2022-09-11 22:04:23 -0700

    Adding in temporary verse example

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h3>Testing out app</h3>
+      <h2>59 One Anothers</h2>
+      <h4>59 Bible Verses that refer to how we are to treat one another</h4>
 
       <div>
-        Verses
+        <span className="verses">&ldquo;Salt is good, but if the salt has lost its saltiness, how will you make it salty again? Have salt in yourselves, and be at peace with one another.&rdquo;</span>
+        <span className="reference">Mark 9:50</span>
       </div>
+
       <footer>
         <p>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. The ESV text may not be quoted in any publication made available to the public by a Creative Commons license. The ESV may not be translated into any other language.</p>
         <p>Users may not copy or download more than 500 verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
