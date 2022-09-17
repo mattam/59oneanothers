26560f957ef63f5e6a89e14ea3cb7a57c08f2c91
Matthew Chan <mattam@gmail.com>
2022-09-16 22:33:02 -0700

    Adjusting the styling of the verses, and adding in snap scroll

--- src/App.js
+++ src/App.js
@@ -32,15 +32,19 @@ const renderVerses = () => {
 function App() {
   return (
     <div className="App">
-      <h2>59 One Anothers</h2>
-      <h4>59 Bible Verses that refer to how we are to treat one another</h4>
-
-      {renderVerses()}
-
-      <footer>
-        <p>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. The ESV text may not be quoted in any publication made available to the public by a Creative Commons license. The ESV may not be translated into any other language.</p>
-        <p>Users may not copy or download more than 500 verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
-      </footer>
+      <div className="verseObj">
+        <h2>59 One Anothers</h2>
+        <span className="verses">
+        <h4>59 Bible Verses about how to treat <span className="oneAnother">one another</span></h4>
+        </span>
+      </div>
+        {renderVerses()}
+      <div className="verseObj">
+        <footer>
+          <p>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. The ESV text may not be quoted in any publication made available to the public by a Creative Commons license. The ESV may not be translated into any other language.</p>
+          <p>Users may not copy or download more than 500 verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
+        </footer>
+      </div>
     </div>
   );
 }
