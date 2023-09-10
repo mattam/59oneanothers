cd24b1532d04247b89893dd8a85b3ff16b4a3064
Matthew Chan <mattam@gmail.com>
2023-09-10 13:32:16 -0700

    Adding in credits

--- src/App.js
+++ src/App.js
@@ -41,6 +41,12 @@ function App() {
         {renderVerses()}
       <div className="verseObj">
         <footer className="verses">
+          <p>Made with ❤️ in foggy San Francisco</p>
+          <p>Curated <a href="https://www.basiltech.org/">BasilTech</a> Bible experience</p>
+        </footer>
+      </div>        
+      <div className="verseObj">
+        <footer className="verses">
           <p>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. The ESV text may not be quoted in any publication made available to the public by a Creative Commons license. The ESV may not be translated into any other language.</p>
           <p>Users may not copy or download more than 500 verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
         </footer>
